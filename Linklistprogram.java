340496fec44a962eba8c2fb563c052206bcf65d0
Ashu Pant <99981051+ashupant01@users.noreply.github.com>
2022-04-14 22:16:28 +0530

    Add files via upload
    Linklist Program

---
+++ Linklistprogram.java
@@ -0,0 +1,23 @@
+package Linklist;
+
+import java.util.LinkedList;
+
+public class Linklistprogram {
+
+	public static void main(String[] args){
+
+	    
+	    LinkedList<String> phones = new LinkedList<>();
+
+	    // phones added
+	    phones.add("Nokia");
+	    phones.add("Samsung");
+	    phones.add("Iphone");
+	    phones.add("Oneplus");
+	    System.out.println("LinkedList is :-> " + phones);
+	  }
+	
+
+
+}
+
